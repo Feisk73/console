46acafec321b195dffbc7663427944618bd1cf4a
Feisk73 <feisk1973@outlook.com>
2025-05-15 00:53:01 +0300

    minor fix

--- src/org/feisk73/consoleApp/commands/EchoCommand.java
+++ src/org/feisk73/consoleApp/commands/EchoCommand.java
@@ -2,6 +2,8 @@ package org.feisk73.consoleApp.commands;
 
 import org.feisk73.consoleApp.ConsoleMain;
 
+import java.util.Arrays;
+
 public class EchoCommand extends CommandBase implements Command {
         public EchoCommand(ConsoleMain main) {
         super(main);
@@ -9,13 +11,8 @@ public class EchoCommand extends CommandBase implements Command {
 
     @Override
     public void execute(String[] args) {
-        StringBuilder output = new StringBuilder();
-        for (String string :
-                args) {
-            output.append(string);
-            output.append(' ');
-        }
-        print(output.toString());
+        String output = Arrays.toString(args);
+        print(output.substring(1, output.length()-1));
     }
 
     @Override
